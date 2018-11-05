a72a97018d61b30a24681eec54447ef251ade7c4
Pete Dring <pddring@googlemail.com>
2018-11-05 21:51:27 +0000

    Initial commit

--- WindowsScalingFix/Form1.Designer.vb
+++ WindowsScalingFix/Form1.Designer.vb
@@ -22,10 +22,129 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnSetDPI = New System.Windows.Forms.Button()
+        Me.btnSetScaleFactor = New System.Windows.Forms.Button()
+        Me.txtDPI = New System.Windows.Forms.TextBox()
+        Me.txtScaleFactor = New System.Windows.Forms.TextBox()
+        Me.chkDPIScaling = New System.Windows.Forms.CheckBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtDPI)
+        Me.GroupBox1.Controls.Add(Me.btnSetDPI)
+        Me.GroupBox1.Location = New System.Drawing.Point(2, 36)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(115, 125)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "DPI for all displays:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.txtScaleFactor)
+        Me.GroupBox2.Controls.Add(Me.btnSetScaleFactor)
+        Me.GroupBox2.Location = New System.Drawing.Point(123, 36)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(120, 125)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Scale factor:"
+        '
+        'btnSetDPI
+        '
+        Me.btnSetDPI.Location = New System.Drawing.Point(21, 84)
+        Me.btnSetDPI.Name = "btnSetDPI"
+        Me.btnSetDPI.Size = New System.Drawing.Size(75, 23)
+        Me.btnSetDPI.TabIndex = 2
+        Me.btnSetDPI.Text = "Set"
+        Me.btnSetDPI.UseVisualStyleBackColor = True
+        '
+        'btnSetScaleFactor
+        '
+        Me.btnSetScaleFactor.Location = New System.Drawing.Point(18, 86)
+        Me.btnSetScaleFactor.Name = "btnSetScaleFactor"
+        Me.btnSetScaleFactor.Size = New System.Drawing.Size(75, 23)
+        Me.btnSetScaleFactor.TabIndex = 3
+        Me.btnSetScaleFactor.Text = "Set"
+        Me.btnSetScaleFactor.UseVisualStyleBackColor = True
+        '
+        'txtDPI
+        '
+        Me.txtDPI.Location = New System.Drawing.Point(6, 19)
+        Me.txtDPI.Name = "txtDPI"
+        Me.txtDPI.Size = New System.Drawing.Size(100, 20)
+        Me.txtDPI.TabIndex = 3
+        '
+        'txtScaleFactor
+        '
+        Me.txtScaleFactor.Location = New System.Drawing.Point(6, 19)
+        Me.txtScaleFactor.Name = "txtScaleFactor"
+        Me.txtScaleFactor.Size = New System.Drawing.Size(100, 20)
+        Me.txtScaleFactor.TabIndex = 4
+        '
+        'chkDPIScaling
+        '
+        Me.chkDPIScaling.AutoSize = True
+        Me.chkDPIScaling.Location = New System.Drawing.Point(13, 13)
+        Me.chkDPIScaling.Name = "chkDPIScaling"
+        Me.chkDPIScaling.Size = New System.Drawing.Size(121, 17)
+        Me.chkDPIScaling.TabIndex = 2
+        Me.chkDPIScaling.Text = "DPI scaling enabled"
+        Me.chkDPIScaling.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 46)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(92, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "96 is normal value"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "0 is normal value"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(251, 199)
+        Me.Controls.Add(Me.chkDPIScaling)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Windows Scaling Fix:"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtDPI As TextBox
+    Friend WithEvents btnSetDPI As Button
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtScaleFactor As TextBox
+    Friend WithEvents btnSetScaleFactor As Button
+    Friend WithEvents chkDPIScaling As CheckBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- WindowsScalingFix/Form1.vb
+++ WindowsScalingFix/Form1.vb
@@ -1,3 +1,38 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReadValuesFromRegistry()
+    End Sub
 
+    Sub ReadValuesFromRegistry()
+        txtScaleFactor.Text = Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "DesktopDPIOverride", "-")
+        txtDPI.Text = Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "LogPixels", "-")
+        chkDPIScaling.Checked = Microsoft.Win32.Registry.GetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "Win8DpiScaling", False)
+    End Sub
+
+    Private Sub btnSetDPI_Click(sender As Object, e As EventArgs) Handles btnSetDPI.Click
+        Try
+            Dim val As Integer = txtDPI.Text
+            Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "LogPixels", val, Microsoft.Win32.RegistryValueKind.DWord)
+        Catch
+            MsgBox("Could not save DPI value")
+        End Try
+
+    End Sub
+
+    Private Sub btnSetScaleFactor_Click(sender As Object, e As EventArgs) Handles btnSetScaleFactor.Click
+        Try
+            Dim val As Integer = txtScaleFactor.Text
+            Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "DesktopDPIOverride", val, Microsoft.Win32.RegistryValueKind.DWord)
+        Catch
+            MsgBox("Could not save DPI value")
+        End Try
+    End Sub
+
+    Private Sub chkDPIScaling_CheckedChanged(sender As Object, e As EventArgs) Handles chkDPIScaling.CheckedChanged
+        Dim val As Int32 = 0
+        If chkDPIScaling.Checked Then
+            val = 1
+        End If
+        Microsoft.Win32.Registry.SetValue("HKEY_CURRENT_USER\Control Panel\Desktop", "Win8DpiScaling", Val, Microsoft.Win32.RegistryValueKind.DWord)
+    End Sub
 End Class
